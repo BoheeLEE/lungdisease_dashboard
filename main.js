279de64a11f8f2d5e7138e2599f1e7a3961a5ba9
Bohee LEE <blee5@ic.ac.uk>
2026-08-18 00:11:29 +0100

    Update main.js
    minor improvements to study link

--- main.js
+++ main.js
@@ -233,6 +233,8 @@ const render = () => {
         cellElement.setAttribute("data-column", key);
         if (key === "study") {
           const link = cellElement.appendChild(document.createElement('a'));
+          link.target = "_blank";
+          link.rel = "noopener noreferrer";
           link.href = row['link'];
           link.textContent = String(row[key]);
         }
